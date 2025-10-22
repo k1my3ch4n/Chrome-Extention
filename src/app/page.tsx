96d7bf90e3e8f968ee1fdb89d2606706270128d8
Kim <kevin2695@naver.com>
2025-10-22 17:23:01 +0900

    initial : next 기본 설정 추가

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "16px", width: "300px" }}>
+      <h1>Hello Extension!</h1>
+      <p>This is a popup rendered by Next.js.</p>
+    </div>
+  );
+}
