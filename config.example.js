7fd83eab17d18e0fcef1f9e0bb97653e93e1d0e4
Kim <kevin2695@naver.com>
2025-10-22 17:18:23 +0900

    delete : delete all package

--- config.example.js
+++
@@ -1,6 +0,0 @@
-// 환경설정 파일 템플릿
-// 실제 사용 시 config.js로 복사하고 실제 값으로 교체하세요
-module.exports = {
-  // GCP에서 발급받은 OAuth 클라이언트 ID
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || 'Google Client ID'
-};
